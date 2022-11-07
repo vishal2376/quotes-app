364c7bc022219ba182ab1cc91828ffd0688a59de
vishal2376 <vishalsingh2376@gmail.com>
2022-11-07 22:49:27 +0530

    mvvm usage

--- app/src/main/java/com/vishal2376/quoteapp/MainActivity.kt
+++ app/src/main/java/com/vishal2376/quoteapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.vishal2376.quoteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import com.vishal2376.quoteapp.models.QuoteList
+import com.vishal2376.quoteapp.viewmodels.QuoteViewModel
+import com.vishal2376.quoteapp.viewmodels.QuoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var quoteViewModel: QuoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repository = (application as QuoteApplication).repository
+
+        quoteViewModel = ViewModelProvider(this,QuoteViewModelFactory(repository))[QuoteViewModel::class.java]
+
+        quoteViewModel.quotes.observe(this){
+            Log.e("@@@",it.toString())
+        }
+
     }
 }
