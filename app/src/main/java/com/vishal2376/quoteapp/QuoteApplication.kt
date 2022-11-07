d419aebcf8af7a7f55f50755f7de277556083ab6
vishal2376 <vishalsingh2376@gmail.com>
2022-11-07 22:40:56 +0530

    QuoteApplication.kt created

---
+++ app/src/main/java/com/vishal2376/quoteapp/QuoteApplication.kt
@@ -0,0 +1,22 @@
+package com.vishal2376.quoteapp
+
+import android.app.Application
+import com.vishal2376.quoteapp.api.QuoteService
+import com.vishal2376.quoteapp.api.RetrofitHelper
+import com.vishal2376.quoteapp.models.QuoteList
+import com.vishal2376.quoteapp.repository.QuoteRepository
+
+class QuoteApplication:Application() {
+
+    lateinit var repository: QuoteRepository
+
+    override fun onCreate() {
+        super.onCreate()
+        initialize()
+    }
+
+    private fun initialize() {
+        val quoteService = RetrofitHelper.getInstance().create(QuoteService::class.java)
+        repository = QuoteRepository(quoteService)
+    }
+}
