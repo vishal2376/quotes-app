57984539e193c754dbd9aab4f6fd951a3867e1e9
vishal2376 <vishalsingh2376@gmail.com>
2022-11-11 00:36:21 +0530

    database created

---
+++ app/src/main/java/com/vishal2376/quoteapp/db/QuoteDatabase.kt
@@ -0,0 +1,30 @@
+package com.vishal2376.quoteapp.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.vishal2376.quoteapp.models.Result
+
+@Database(entities = [Result::class], version = 1)
+abstract class QuoteDatabase : RoomDatabase() {
+    abstract fun quoteDao(): QuoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: QuoteDatabase? = null
+
+        fun getDatabase(context: Context): QuoteDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        QuoteDatabase::class.java,
+                        "quoteDB"
+                    ).build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
