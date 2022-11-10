5017ee80daf9df310c28f71b3cead819dd44eaf0
vishal2376 <vishalsingh2376@gmail.com>
2022-11-11 00:29:03 +0530

    dao created

---
+++ app/src/main/java/com/vishal2376/quoteapp/db/QuoteDao.kt
@@ -0,0 +1,15 @@
+package com.vishal2376.quoteapp.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.vishal2376.quoteapp.models.Result
+
+@Dao
+interface QuoteDao {
+    @Insert
+    suspend fun addQuote(quote: Result)
+
+    @Query("SELECT * FROM quote")
+    suspend fun getQuotes(): List<Result>
+}
