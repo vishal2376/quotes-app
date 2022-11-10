e64ca8eaadccef5d8ac8c321a913d862a573901b
vishal2376 <vishalsingh2376@gmail.com>
2022-11-11 00:23:56 +0530

    entities added

--- app/src/main/java/com/vishal2376/quoteapp/models/Result.kt
+++ app/src/main/java/com/vishal2376/quoteapp/models/Result.kt
@@ -1,12 +1,17 @@
 package com.vishal2376.quoteapp.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "quote")
 data class Result(
+    @PrimaryKey(autoGenerate = true)
+    val quoteId: Int,
     val _id: String,
     val author: String,
     val authorSlug: String,
     val content: String,
     val dateAdded: String,
     val dateModified: String,
-    val length: Int,
-    val tags: List<String>
+    val length: Int
 )
